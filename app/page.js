1b9d48774b62f0d7d539d5ea9fd1f17072681826
zagzook <ja0213@gmail.com>
2025-01-25 18:30:01 -0500

    updated hero section

--- app/page.js
+++ app/page.js
@@ -3,7 +3,11 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main>
-      <button className='btn'>Start Now</button>
+      <h1>Collect customer feedback to build better products</h1>
+      <div>
+        Create a feedback board in minutes, prioritize features, and build
+        product your customers will love.
+      </div>
     </main>
   );
 }
